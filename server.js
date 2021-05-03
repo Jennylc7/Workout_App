0ae357ad78070d4fbb9d395bfbbcde7da420ffc5
jennylc7 <thepilatesnut@gmail.com>
2021-05-03 13:07:24 -0600

    workout app

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3001
 
 const app = express();
 
@@ -15,7 +15,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout_db", {
   useFindAndModify: false
 });
 
-
+app.use(require("./routes/api-routes.js"))
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
